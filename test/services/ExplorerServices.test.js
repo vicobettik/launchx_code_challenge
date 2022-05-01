3a1cda79a305726081e88e2f26b69beb8f738a38
Victor Fuentes Menes <vicuo@hotmail.com>
2022-05-01 11:47:58 -0500

    se agrega archivo de explorers para pruebas, se agregan metodos en Explorerservice y pruebas de los mismos

--- test/services/ExplorerServices.test.js
+++ test/services/ExplorerServices.test.js
@@ -1,25 +1,31 @@
 const ExplorerService = require('../../lib/services/ExplorerService');
+const Reader = require('../../lib/utils/Reader');
 
 describe('Test for ExplorerService', () => {
   
     test('1) Return all explorers with all properties', () => {
-      
-        const explorers = [
-            {
-                name: "explorer1",
-                prop1: "prop1"
-            },
-            {
-                name: "explorer2",
-                prop2: "prop2"
-            }
-        ];
 
+        const explorers = Reader.readJsonFile('explorerTest.json');
         const allExplorers = ExplorerService.getAllExplorers(explorers);
-        expect(allExplorers.length).not.toBe(0)
+        expect(allExplorers.length).not.toBe(0);
 
 
     })
+
+    test('2) return all mail of explorers who has certification', () => {
+      
+        const explorers = Reader.readJsonFile('explorerTest.json');
+        const mailOfExplorersWithCertification = ExplorerService.getEmailsExplorersWithCertification(explorers);
+        expect(mailOfExplorersWithCertification.length).toBe(1);
+
+    })
     
+    test('3) return all explorers who has credits greater than 500', () => {
+      
+        const explorers = Reader.readJsonFile('explorerTest.json');
+        const mailOfExplorersWithCertification = ExplorerService.getExplorersWithCreditsGreaterThan500(explorers);
+        expect(mailOfExplorersWithCertification.length).toBe(1);
+
+    })
 
 })
