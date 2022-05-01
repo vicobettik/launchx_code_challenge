20006d4da3161a245d882e405be4981322745445
Victor Fuentes Menes <vicuo@hotmail.com>
2022-05-01 11:26:49 -0500

    se agrega metodo al servicio para obtener los correos de los explorers

--- index.js
+++ index.js
@@ -2,5 +2,9 @@ const Reader = require('./lib/utils/Reader');
 const ExplorerService = require('./lib/services/ExplorerService');
 
 const explorers = Reader.readJsonFile('explorers.json');
+
 const allExplorers = ExplorerService.getAllExplorers(explorers);
-console.log(allExplorers)
+// console.log(allExplorers)
+
+const explorersMails = ExplorerService.getEmailsExplorersWithCertification(explorers);
+console.log(explorersMails);
